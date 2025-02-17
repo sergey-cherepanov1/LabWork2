becccf845e21d8bd46fda4ce9b1c3feb1e6ddbd8
kaido <st129987@student.spbu.ru>
2025-02-17 23:14:56 +0300

    created a structure for the future project, made a proper makefile, configured the CI and prepared for tests.

---
+++ tests/test.cpp
@@ -0,0 +1,11 @@
+/* Sergey Cherepanov st129987@student.spbu.ru
+   LabWork1
+*/
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
