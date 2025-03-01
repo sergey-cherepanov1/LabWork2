9e84c0e38a69bd87fdd91d1d674d08402840b967
kaido <st129987@student.spbu.ru>
2025-03-01 13:39:40 +0300

    Added comments to main.cpp and test.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 /* Sergey Cherepanov st129987@student.spbu.ru
    LabWork2
 */
-int main()
-{
 
+// src/main.cpp
+#include <iostream>
+
+/// @brief Main entry point of the game application.
+/// @return Exit status (0 for success).
+int main() {
+    std::cout << "Welcome to the game!" << std::endl;
+    return 0;
 }
